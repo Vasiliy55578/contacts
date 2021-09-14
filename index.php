e8c6429145e563a9d7ea4aef023d070ba207386a
Vasiliy <63239498+Vasiliy55578@users.noreply.github.com>
2021-09-15 02:23:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Page Contacts</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form action="send.php" method="post">
+    <div class="space">
+	    <p>Имя</p>
+	    <input type="text" name="name">
+    </div>
+  <div class="space">
+    <p>Телефон</p>
+    <input type="text" name="phone">
+  </div>
+  <div class="space">
+    <p>Email</p>
+    <input type="text" name="email">
+  </div>
+   <button id="send" type="submit">Отправить</button>
+</form>
+</body>
+</html>
